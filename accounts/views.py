47079e4a7c53ece923d1b3706e4c5c804bfadb6a
Engr. Peter Goteh <edwardpeter547@gmail.com>
2022-06-13 19:06:54 +0100

    updated login.html template to use AuthenticationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
@@ -8,17 +8,19 @@ def login_view(request):
     context = {}
     
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        
-        user = authenticate(request=request, username = username, password = password)
-        if not user:
-            context["error"] = "username or password is incorrect"
-            return render(request=request, template_name="accounts/login.html", context=context)
-        login(request=request, user=user)
-        return redirect("/")
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request=request, user=user)
+            return redirect("/")
+        else:
+            return render(request=request, template_name="accounts/login.html", context={"form": form})
     
+    form = AuthenticationForm(request)    
+    context.update({"form": form})
     return render(request=request, template_name="accounts/login.html", context=context)
+
+
 
 def logout_view(request):
     
