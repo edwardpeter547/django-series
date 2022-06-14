22553c79ab60de3dcbc33b08c480aae50ee315b4
Engr. Peter Goteh <edwardpeter547@gmail.com>
2022-06-14 13:44:25 +0100

    removed the secret key from the settings.py

--- web/settings.py
+++ web/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-@6=xr$m!v#jdndid76no)mf-g-+@+00_-i!darg0-0#id6(8+5'
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
